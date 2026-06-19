7b76362ca89816b63f3dda5874a65aba76767dc4
Damodhar Reddy K <damodhar.reddy@contentstack.com>
2026-06-19 23:10:20 +0530

    Added signature to the receipts and minor cosmetic changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.o43eb53oco8"
+    "revision": "0.qdeu429nlmo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
